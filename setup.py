208806c2c280e3fe0a8876d1a24285a47a363256
Matthias Gazzari <mail@qtux.eu>
2019-11-25 15:43:38 +0100

    Add missing dependencies in setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,16 @@ setup(
     long_description='Fast CPU/CUDA implementation of the Continuous Wavelet Transform in PyTorch.',
     license='MIT',
     packages=['wavelets_pytorch'],
+    install_requires=[
+        'torch>=0.4.0',
+        'numpy>=1.14.1',
+        'scipy>=1.0.0',
+    ],
+    python_requires='>2.7,' + ",".join(["!=3.%s.*" % i for i in range(6)]),
+    extras_require={
+        'examples':[
+            'matplotlib>=2.0.0',
+        ],
+    },
     scripts=[]
 )
